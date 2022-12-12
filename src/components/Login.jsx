5095c1ab6fb69ca6f664372bd5586bc55d962561
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-13 01:02:26 +0300

    coode

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,13 @@ const Login = () => {
         <Content>
             <CTA>
                 <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                <SignUp>GET ALL THERE</SignUp>
+                <Description>
+                    Get Premier Access to Raya and the Last Dragon for an additional fee
+                    with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                    and The Disney Bundle will increase by $1.
+                </Description>
+                <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
             </CTA>
             <BgImage />
         </Content>
@@ -62,5 +69,36 @@ const CTALogoOne = styled.img`
   max-width: 600px;
   min-height: 1px;
   display: block;
+  width: 100%;
+`
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
   width: 100%;
 `
