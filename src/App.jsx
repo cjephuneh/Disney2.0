6e2820e94723eb1e064f6ed0e3d6ba71ea0d95e4
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-13 01:06:01 +0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Login from './components/Login'
+import Header from './components/Header'
+
 
 
 function App() {
    return (
-  
-        <Login />
-   
+    <>
+      <Header />  
+      <Login />
+    </>
+    
   )
 }
 
