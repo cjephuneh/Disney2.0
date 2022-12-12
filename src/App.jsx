6f6f96464bcf6c2f4364b3d7e22b19058a6c03f6
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-13 00:50:09 +0300

    commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Login from "./components/Login"
+import Login from './components/Login'
 
 
 function App() {
    return (
-   <Router>
-    <Routes>
-      <Route exact path='/'>
+  
         <Login />
-      </Route>
-    </Routes>
-   </Router>
+   
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 const Login = () => {
   return (
-    <div>
-      <p>this is the log in page</p>
-    </div>
+    <Container>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+            </CTA>
+            <BgImage />
+        </Content>
+    </Container>
   )
 }
 
 export default Login
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+`
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`
